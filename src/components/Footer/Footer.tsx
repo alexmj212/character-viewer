65bd5e4756f56883b020ff8c9b5fa334ef56a3a1
Alex Johnson <alexmj212@gmail.com>
2019-06-19 22:00:03 -0400

    fix console warning

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ class Footer extends React.Component {
     render() {
         return (
             <footer className={styles.footer}>
-                <a target="_blank" href="https://github.com/alexmj212">alexmj212</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/alexmj212">alexmj212</a>
             </footer>
         )
     }
