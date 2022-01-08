722e683469f9f5fe8a3df814cec2f5b19cd3ee6d
Alex Johnson <alexmj212@gmail.com>
2022-01-07 23:13:12 -0500

    recreate app

--- src/interfaces/Class.interface.tsx
+++
@@ -1,9 +0,0 @@
-import { Abilities } from "../enums/Abilites.enum";
-
-export interface BaseClassInterface {
-    name: string,
-    description: string,
-    hitDie: number,
-    primaryAbility: Abilities[],
-    saves: Abilities[]
-}
--- src/interfaces/Stats.interface.tsx
+++
@@ -1,8 +0,0 @@
-export interface BaseStats {
-    STR: number;
-    DEX: number;
-    CON: number;
-    WIS: number;
-    INT: number;
-    CHA: number;
-}
